685a7ad216594d580f8d5248aae12f3eb3019346
MinPan9585 <panmin677095@gmail.com>
2025-11-16 21:19:34 +0800

    111

---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    public PlayerOneMove playerOneMove;
+
+    private void Start()
+    {
+        playerOneMove = GetComponentInParent<PlayerOneMove>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            playerOneMove.isGrounded = true;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            playerOneMove.isGrounded = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerOneMove.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOneMove : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float leftRightSpeed = 5f;
+    public float upSpeed = 10f;
+    public bool isGrounded;
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-leftRightSpeed, rb.velocity.y);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(leftRightSpeed, rb.velocity.y);
+        }
+
+        if(Input.GetKey(KeyCode.UpArrow) && isGrounded) 
+        {
+            rb.velocity = new Vector2(rb.velocity.x, upSpeed);
+        }
+
+    }
+}
