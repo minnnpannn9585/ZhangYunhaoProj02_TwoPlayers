68a53f94991355e76ad14be667876485bbf390a0
MinPan9585 <panmin677095@gmail.com>
2025-12-28 21:56:14 +0800

    1228

--- Assets/TESTGAME/Scene1/C#/PlatformMove.cs
+++ Assets/TESTGAME/Scene1/C#/PlatformMove.cs
@@ -36,6 +36,19 @@ public class PlatformMove : MonoBehaviour
             rb.velocity = new Vector2(0f, 0f);
             return;
         }
+        if (isRotate == true)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                rotation = rotationSpeed * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                rotation = -rotationSpeed * Time.deltaTime;
+            }
+            transform.Rotate(0f, 0f, rotation);
+        }
+        else
         {
             if (Input.GetKey(KeyCode.RightArrow))
             {
@@ -61,38 +74,53 @@ public class PlatformMove : MonoBehaviour
             {
                 rb.velocity = new Vector2(0f, 0f);
             }
-            if (isRotate == true)
+        }
+
+        if (isRotate == true)
+        {
+            if (transform.position.x < minX + transform.localScale.x / 2)
             {
-                if (Input.GetKey(KeyCode.K))
-                {
-                    rotation = rotationSpeed * Time.deltaTime;
-                }
-                else if (Input.GetKey(KeyCode.L))
-                {
-                    rotation = -rotationSpeed * Time.deltaTime;
-                }
-                transform.Rotate(0f, 0f, rotation);
+                transform.position = new Vector3(minX + transform.localScale.x / 2, transform.position.y, transform.position.z);
+            }
+            if (transform.position.x > maxX - transform.localScale.x / 2)
+            {
+                transform.position = new Vector3(maxX - transform.localScale.x / 2, transform.position.y, transform.position.z);
             }
 
+            if (transform.position.y < minY + transform.localScale.x / 2)
+            {
+                transform.position = new Vector3(transform.position.x, minY + transform.localScale.x / 2, transform.position.z);
+            }
+
+            if (transform.position.y > maxY - transform.localScale.x / 2)
+            {
+                transform.position = new Vector3(transform.position.x, maxY - transform.localScale.x / 2, transform.position.z);
+            }
+        }
+        else
+        {
+            if (transform.position.x < minX + transform.localScale.x / 2)
+            {
+                transform.position = new Vector3(minX + transform.localScale.x / 2, transform.position.y, transform.position.z);
+            }
+            if (transform.position.x > maxX - transform.localScale.x / 2)
+            {
+                transform.position = new Vector3(maxX - transform.localScale.x / 2, transform.position.y, transform.position.z);
+            }
+
+            if (transform.position.y < minY + transform.localScale.y / 2)
+            {
+                transform.position = new Vector3(transform.position.x, minY + transform.localScale.y / 2, transform.position.z);
+            }
+
+            if (transform.position.y > maxY - transform.localScale.y / 2)
+            {
+                transform.position = new Vector3(transform.position.x, maxY - transform.localScale.y / 2, transform.position.z);
+            }
         }
 
-        if (transform.position.x < minX)
-        {
-            transform.position = new Vector3(minX, transform.position.y, transform.position.z);
-        }
-        if (transform.position.x > maxX)
-        {
-            transform.position = new Vector3(maxX, transform.position.y, transform.position.z);
-        }
 
-        if (transform.position.y < minY)
-        {
-            transform.position = new Vector3(transform.position.x, minY, transform.position.z);
-        }
 
-        if (transform.position.y > maxY)
-        {
-            transform.position = new Vector3(transform.position.x, maxY, transform.position.z);
-        }
+            
     }
 }
