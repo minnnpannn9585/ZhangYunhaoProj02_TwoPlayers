f38af872c9bb0ae89c25d9f9a6fd7e79ebb9bc26
xiaobai555555 <160624844+xiaobai555555@users.noreply.github.com>
2026-01-18 21:01:44 +0800

    111

--- Assets/TESTGAME/Scene1/Scripts/PlatformMove.cs
+++ Assets/TESTGAME/Scene1/Scripts/PlatformMove.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlatformMove : MonoBehaviour
@@ -12,14 +13,15 @@ public class PlatformMove : MonoBehaviour
     float rotation = 0f;
     [HideInInspector]
     public bool isSelected;
-
+    public bool isPlayer;
+    public bool isScale;
     public Transform pointOne;
     public Transform pointTwo;
     private float minX;
     private float minY;
     private float maxX;
     private float maxY;
-
+    public float scale;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -27,100 +29,131 @@ public class PlatformMove : MonoBehaviour
         minY = pointOne.position.y;
         maxX = pointTwo.position.x;
         maxY = pointTwo.position.y;
+        isPlayer = false;
     }
-
     void Update()
     {
-        if (!isSelected)
+        if (isPlayer)
         {
-            rb.velocity = new Vector2(0f, 0f);
-            return;
+            if (!isSelected)
+            {
+                rb.velocity = new Vector2(0f, 0f);
+                return;
+            }
+            if (isRotate == true)
+            {
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    rotation = rotationSpeed * Time.deltaTime;
+                }
+                else if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    rotation = -rotationSpeed * Time.deltaTime;
+                }
+                transform.Rotate(0f, 0f, rotation);
+            }
+            else
+            {
+                if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    rb.velocity = new Vector2(speed, 0f);
+                }
+                else if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    rb.velocity = new Vector2(-speed, 0f);
+                }
+                if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
+                {
+                    rb.velocity = new Vector2(0f, 0f);
+                }
+                if (Input.GetKey(KeyCode.UpArrow))
+                {
+                    rb.velocity = new Vector2(0f, speed);
+                }
+                else if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    rb.velocity = new Vector2(0f, -speed);
+                }
+                if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow))
+                {
+                    rb.velocity = new Vector2(0f, 0f);
+                }
+            }
+
+            if (isRotate == true)
+            {
+                if (transform.position.x < minX + transform.localScale.x / 2)
+                {
+                    transform.position = new Vector3(minX + transform.localScale.x / 2, transform.position.y, transform.position.z);
+                }
+                if (transform.position.x > maxX - transform.localScale.x / 2)
+                {
+                    transform.position = new Vector3(maxX - transform.localScale.x / 2, transform.position.y, transform.position.z);
+                }
+
+                if (transform.position.y < minY + transform.localScale.x / 2)
+                {
+                    transform.position = new Vector3(transform.position.x, minY + transform.localScale.x / 2, transform.position.z);
+                }
+
+                if (transform.position.y > maxY - transform.localScale.x / 2)
+                {
+                    transform.position = new Vector3(transform.position.x, maxY - transform.localScale.x / 2, transform.position.z);
+                }
+            }
+            else
+            {
+                if (transform.position.x < minX + transform.localScale.x / 2)
+                {
+                    transform.position = new Vector3(minX + transform.localScale.x / 2, transform.position.y, transform.position.z);
+                }
+                if (transform.position.x > maxX - transform.localScale.x / 2)
+                {
+                    transform.position = new Vector3(maxX - transform.localScale.x / 2, transform.position.y, transform.position.z);
+                }
+
+                if (transform.position.y < minY + transform.localScale.y / 2)
+                {
+                    transform.position = new Vector3(transform.position.x, minY + transform.localScale.y / 2, transform.position.z);
+                }
+
+                if (transform.position.y > maxY - transform.localScale.y / 2)
+                {
+                    transform.position = new Vector3(transform.position.x, maxY - transform.localScale.y / 2, transform.position.z);
+                }
+            }
+            if (isScale == true) {
+                if (Input.GetKey(KeyCode.J))
+                    transform.localScale = new Vector3(scale, scale,scale);
+                if (Input.GetKey(KeyCode.K))
+                    transform.localScale = new Vector3(transform.localScale.x/scale, transform.localScale.y / scale, transform.localScale.z / scale);
+            } 
+            
+
+
+
+
         }
-        if (isRotate == true)
-        {
-            if (Input.GetKey(KeyCode.LeftArrow))
+        else {
+            if (isRotate == true)
             {
                 rotation = rotationSpeed * Time.deltaTime;
             }
-            else if (Input.GetKey(KeyCode.RightArrow))
+
+            if (isRotate == false)
             {
-                rotation = -rotationSpeed * Time.deltaTime;
+                rb.velocity = new Vector2(speed, 0f);
+                if (transform.position.x < minX + transform.localScale.x / 2)
+                {
+                    rb.velocity = new Vector2(speed, 0f);
+                }
+                if (transform.position.x > maxX - transform.localScale.x / 2)
+                {
+                    rb.velocity = new Vector2(-speed, 0f);
+                }
             }
             transform.Rotate(0f, 0f, rotation);
         }
-        else
-        {
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                rb.velocity = new Vector2(speed, 0f);
-            }
-            else if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                rb.velocity = new Vector2(-speed, 0f);
-            }
-            if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
-            {
-                rb.velocity = new Vector2(0f, 0f);
-            }
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-                rb.velocity = new Vector2(0f, speed);
-            }
-            else if (Input.GetKey(KeyCode.DownArrow))
-            {
-                rb.velocity = new Vector2(0f, -speed);
-            }
-            if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow))
-            {
-                rb.velocity = new Vector2(0f, 0f);
-            }
-        }
-
-        if (isRotate == true)
-        {
-            if (transform.position.x < minX + transform.localScale.x / 2)
-            {
-                transform.position = new Vector3(minX + transform.localScale.x / 2, transform.position.y, transform.position.z);
-            }
-            if (transform.position.x > maxX - transform.localScale.x / 2)
-            {
-                transform.position = new Vector3(maxX - transform.localScale.x / 2, transform.position.y, transform.position.z);
-            }
-
-            if (transform.position.y < minY + transform.localScale.x / 2)
-            {
-                transform.position = new Vector3(transform.position.x, minY + transform.localScale.x / 2, transform.position.z);
-            }
-
-            if (transform.position.y > maxY - transform.localScale.x / 2)
-            {
-                transform.position = new Vector3(transform.position.x, maxY - transform.localScale.x / 2, transform.position.z);
-            }
-        }
-        else
-        {
-            if (transform.position.x < minX + transform.localScale.x / 2)
-            {
-                transform.position = new Vector3(minX + transform.localScale.x / 2, transform.position.y, transform.position.z);
-            }
-            if (transform.position.x > maxX - transform.localScale.x / 2)
-            {
-                transform.position = new Vector3(maxX - transform.localScale.x / 2, transform.position.y, transform.position.z);
-            }
-
-            if (transform.position.y < minY + transform.localScale.y / 2)
-            {
-                transform.position = new Vector3(transform.position.x, minY + transform.localScale.y / 2, transform.position.z);
-            }
-
-            if (transform.position.y > maxY - transform.localScale.y / 2)
-            {
-                transform.position = new Vector3(transform.position.x, maxY - transform.localScale.y / 2, transform.position.z);
-            }
-        }
-
-
-
-            
     }
+    
 }
